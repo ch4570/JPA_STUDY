d5a37c9291b1a482ca1fe8ed00b142e5375a60a6
DevSeoRex <ch4570@naver.com>
2022-11-22 22:25:13 +0900

    JPA - Proxy Study

---
+++ src/main/java/jpashop/domain/JpaMain4.java
@@ -0,0 +1,37 @@
+package jpashop.domain;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain4 {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        // EntityManager는 데이터 변경시 트랜잭션을 시작해야 한다.
+
+        tx.begin();  // [트랜잭션] 시작
+
+        try{
+
+            Book book = new Book();
+            book.setName("JPA");
+            book.setIsbn("김영한");
+
+            em.persist(book);
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
